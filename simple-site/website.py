eb816a123a8bbaac988ead37f27f8e178527fa4a
Caio Gandra <caio.gandra@mandae.com.br>
2017-05-12 18:32:18 -0300

    New requirements and added necessary lines.

--- simple-site/website.py
+++ simple-site/website.py
@@ -13,6 +13,7 @@ def plot():
     cdn = candlechart.fetch_cdn()
 
     return render_template("plot.html", cdn_css = cdn["cdn_css"], cdn_js = cdn["cdn_js"],
+    script=comps["script"], div=comps["div"])
 
 @app.route('/about')
 def about():
